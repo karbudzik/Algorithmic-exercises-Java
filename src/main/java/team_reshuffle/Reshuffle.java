cd80d08523039b0f1a0d29ffe1661f6340935ba5
karbudzik <karolinabudzik92@gmail.com>
2020-07-23 20:00:27 +0200

    Start working on generateNewTeams() method

--- src/main/java/team_reshuffle/Reshuffle.java
+++ src/main/java/team_reshuffle/Reshuffle.java
@@ -11,9 +11,38 @@ public class Reshuffle {
     private final List<Team> oldTeams = new ArrayList<>();
     private final List<Team> newTeams = new ArrayList<>();
 
-    public void letsRoll() {
-        System.out.println("Some magic happens here");
+    public void generateNewTeams(int maxNumberOfStudentsInTeam) {
+        int oldTeamsCount = oldTeams.size();
+        int allStudentsCount = oldTeams.stream()
+                .reduce(0, (partialResult, team) -> partialResult + team.getMembers().size(), Integer::sum);
+        if (maxNumberOfStudentsInTeam > oldTeamsCount) {
+            throw new IllegalArgumentException("The number of");
+        }
+        int numberOfStudentsLeft = allStudentsCount;
+
+        while (numberOfStudentsLeft > 0) {
+            Team newTeam = new Team();
+            int studentsNumberInTeam = Math.min(maxNumberOfStudentsInTeam, numberOfStudentsLeft);
+            int teamIndex = 0;
+            for(int i = 1; i <= studentsNumberInTeam; i++) {
+                Team team = oldTeams.get(teamIndex);
+                
+            }
+            numberOfStudentsLeft -= studentsNumberInTeam;
+        }
     }
+
+
+
+//    pętlaWPętli (powtarzana razy numOfStudInTeam):
+//    biorę team 0
+//    sprawdzam czy ma elementy
+//    jeśli tak to losuję randomowego Stringa i go stamtąd wycinam, i wrzucam do newTeam
+//    jeśli nie to idę do kolejnego teamu
+//    przechodzę do teamu 1 etc.
+//odejmowanie od numberOfStudentsLeft
+//    po zakończeniu pętli w pętli, wrzucam team do listyteamów i sprawdzam czy jacyś studenci zostali
+
 
     public void loadOldTeams(String fileName) {
         oldTeams.clear();
@@ -48,9 +77,9 @@ public class Reshuffle {
 
     public static void main(String[] args) {
         Reshuffle reshuffle = new Reshuffle();
-        reshuffle.loadOldTeams("resources/teams_sample.txt");
+        reshuffle.loadOldTeams("src/main/resources/team_reshuffle/teams_sample.txt");
         reshuffle.printOldTeams();
-        reshuffle.letsRoll();
+        reshuffle.generateNewTeams(4);
         reshuffle.printNewTeams();
     }
 
